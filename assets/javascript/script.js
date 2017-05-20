06aaa5ccf6fd94c5df8332d1ff23a64c8a3f517a
Spencer-P-Brown <Spencer-P-Brown@github.com>
2017-05-20 12:58:03 -0500

    troubleshooting some errors

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
 	function displayGif() {
 
        var crazyPerson = $(this).attr("data-name");
-       var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
            crazyPerson + "&api_key=dc6zaTOxFJmzC&limit=10";
 
        $.ajax({
